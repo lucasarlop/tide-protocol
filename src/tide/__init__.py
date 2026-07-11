e500cc0858c8b593e34292ec2fb32e5ae1baf09e
Lucas Lopes <lucas.arlop@gmail.com>
2026-07-11 14:25:53 -0300

    fix: install segmentation refinements

--- src/tide/__init__.py
+++ src/tide/__init__.py
@@ -6,9 +6,11 @@ from . import core as _core
 from .agility import install as _install_agility
 from .closure import install as _install_closure
 from .guardrails import install as _install_guardrails
+from .segmentation import install as _install_segmentation
 
 _install_guardrails(_core)
 _install_agility(_core)
 _install_closure(_core)
+_install_segmentation(_core)
 
-del _core, _install_guardrails, _install_agility, _install_closure
+del _core, _install_guardrails, _install_agility, _install_closure, _install_segmentation
